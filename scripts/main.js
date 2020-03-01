0283d972d99709fecfcb26ba7c08c6f00af0dff3
Edwin <edch90@gmail.com>
2020-03-01 16:03:29 -0500

    Versión final. Mejoramiento de Javascript

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,13 @@
 // Atrapa el botón y añade la clase is active al dropdown
 
 function clicenboton(element) {
-  this.element = document.querySelector(element);
-  this.element.onclick = () => {
-    this.element.nextElementSibling.classList.toggle("is-active");
+  let button = document.querySelector(element);
+  button.onclick = () => {
+    button.nextElementSibling.classList.toggle("is-active");
   };
 }
 // Previene que haya más de un dropdown abierto
+
 function clearClasses(target) {
   document.querySelectorAll(".dropdown-content.is-active").forEach(item => {
     if (event.target.nextElementSibling !== item)
@@ -14,44 +15,42 @@ function clearClasses(target) {
   });
 }
 // Cierra el dropdown si el usuario hace clic fuera
+
 window.onclick = function(e) {
   clearClasses(event.target.nextElementSibling);
 };
-// Variables para los botones
 
-let agua = new clicenboton(".js-Agua");
-let fuego = new clicenboton(".js-Fuego");
-let tierra = new clicenboton(".js-Tierra");
-let electrico = new clicenboton(".js-Eléctrico");
-let fantasma = new clicenboton(".js-Fantasma");
-let psiquico = new clicenboton(".js-Psíquico");
-let veneno = new clicenboton(".js-Veneno");
-let siniestro = new clicenboton(".js-Siniestro");
-let bicho = new clicenboton(".js-Bicho");
-let roca = new clicenboton(".js-Roca");
-let hielo = new clicenboton(".js-Hielo");
-let dragon = new clicenboton(".js-Dragón");
-let hada = new clicenboton(".js-Hada");
-let acero = new clicenboton(".js-Acero");
-let lucha = new clicenboton(".js-Lucha");
-let normal = new clicenboton(".js-Normal");
-let volador = new clicenboton(".js-Volador");
-let hierba = new clicenboton(".js-Hierba");
+// Función para iterar botones en la función clic en botón
 
-// Imágenes con clic en los dropdown
+function IterarBotones() {
+  let conteo2 = document.querySelectorAll("button");
+  let contearray2 = Array.from(conteo2);
+  for (i = 0; i < contearray2.length; i++) {
+    let resultado2 = contearray2[i].innerHTML;
+    clicenboton(`.js-${resultado2}`);
+  }
+}
 
+IterarBotones();
+
+// Función para cambiar las imágenes y nombres de los pokemon
 
 function cambiarArticuloPkm(element) {
-    let li = document.querySelector(element);
-  li.addEventListener("click", function(){
+  let li = document.querySelector(element);
+  li.addEventListener("click", function() {
     document.querySelector(".name-pkmn").innerHTML = li.innerHTML.toUpperCase();
     document.getElementById("img-pkmn").src = `images/pkmn/${li.innerHTML}.png`;
+  });
+}
 
-  })
+// Para iterar cada nombre de pokemon en la función cambiarArticuloPkm
+
+function IterarNombres() {
+  let conteo = document.querySelectorAll("li");
+  let contearray = Array.from(conteo);
+  for (i = 0; i < contearray.length; i++) {
+    let resultado = contearray[i].innerHTML;
+    cambiarArticuloPkm(`.${resultado}`);
   }
-
-// Variables para cambiar la imagen y el texto
-let squirtle = new cambiarArticuloPkm(".Squirtle")
-let kyogre = new cambiarArticuloPkm(".Kyogre")
-let milotic = new cambiarArticuloPkm(".Milotic")
-let mudkip = new cambiarArticuloPkm(".Mudkip")
+}
+IterarNombres();
