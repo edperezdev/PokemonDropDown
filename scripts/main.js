6235beceb7a6144de9fafc41e009e3bbe5958098
Edwin <edch90@gmail.com>
2020-03-01 11:58:27 -0500

    Añadida funcionalidad al hacer clic en cada pokemon, aparece su imagen

--- scripts/main.js
+++ scripts/main.js
@@ -7,16 +7,16 @@ function clicenboton(element) {
   };
 }
 // Previene que haya más de un dropdown abierto
-function clearClasses (target){
-  document.querySelectorAll('.dropdown-content.is-active').forEach((item)=>{
-    if(event.target.nextElementSibling !== item)
-      item.classList.remove('is-active');
-  })
+function clearClasses(target) {
+  document.querySelectorAll(".dropdown-content.is-active").forEach(item => {
+    if (event.target.nextElementSibling !== item)
+      item.classList.remove("is-active");
+  });
 }
 // Cierra el dropdown si el usuario hace clic fuera
 window.onclick = function(e) {
   clearClasses(event.target.nextElementSibling);
-}
+};
 // Variables para los botones
 
 let agua = new clicenboton(".js-Agua");
@@ -37,3 +37,21 @@ let lucha = new clicenboton(".js-Lucha");
 let normal = new clicenboton(".js-Normal");
 let volador = new clicenboton(".js-Volador");
 let hierba = new clicenboton(".js-Hierba");
+
+// Imágenes con clic en los dropdown
+
+
+function cambiarArticuloPkm(element) {
+    let li = document.querySelector(element);
+  li.addEventListener("click", function(){
+    document.querySelector(".name-pkmn").innerHTML = li.innerHTML.toUpperCase();
+    document.getElementById("img-pkmn").src = `images/pkmn/${li.innerHTML}.png`;
+
+  })
+  }
+
+// Variables para cambiar la imagen y el texto
+let squirtle = new cambiarArticuloPkm(".Squirtle")
+let kyogre = new cambiarArticuloPkm(".Kyogre")
+let milotic = new cambiarArticuloPkm(".Milotic")
+let mudkip = new cambiarArticuloPkm(".Mudkip")
